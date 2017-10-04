752738bb5198e2c22518e2efaa5a268df9f447c0
Robert Bunch <rob@digitalcrafts.com>
2017-10-04 13:59:17 -0400

    loop through all inputs to check for non-empty fields

--- validate.js
+++ validate.js
@@ -11,12 +11,53 @@ $(document).ready(()=>{
 		event.preventDefault();
 		console.log("User submitted the form!");
 		// The fullName needs to be at least 2 characters
-		var userName = $('.fullName').val();
-		if(userName.length < 2){
-			// console.log("USER DIDNT ENTER THEIR REAL NAME (Unless it's T)")
-			$('.full-name-error').show();
-		}else{
-			$('.full-name-error').hide();
+		// Input tags have values. Almost everything else has innerHTML
+
+		// Target all the input fields...
+		// loop through all, and check the length...
+
+		// console.log(($('.form-wrapper input')))
+
+		// .map((inputTagMapIsOn,i)=>{
+		// 	console.log(inputTagMapIsOn)
+		// })
+
+		$('.form-wrapper input').each(function(){
+			// console.log($(this).val(''));
+			// get the class of the thing we are on.
+			// i.e., password, adress, phone, etc.
+			// and IF it's empty, go grab the thing with the class
+			// that matches it + '-error'
+			var userInput = $(this).val();
+			var thisClass = $(this).attr('class');
+			// console.log(`${thisClass} has a value of ${userInput}`)
+			// console.log(userInput);
+			var errorClass = `.${thisClass}-error`;
+			if(userInput === ""){
+				// if we dont give .html() any arg, it will GET the contents
+				// if we send an arg to .html() it will SET the contents to the arg
+				$(errorClass).html("Field must not be empty.")
+				$(errorClass).show();
+				// $('.email-erro').html()
+			}else{
+				// $(errorClass).html("")
+				$(errorClass).hide();
+			}
+		});
+
+		// Get the value of the password input
+		var password = $('.password').val();
+		var password2 = $('.password2').val();
+		if(password !== password2){
+			$('.password-error').html('Your passwords do not match.')
 		}
+
+		// var userName = $('.full-name').val();
+		// if(userName.length < 2){
+		// 	// console.log("USER DIDNT ENTER THEIR REAL NAME (Unless it's T)")
+		// 	$('.full-name-error').show();
+		// }else{
+		// 	$('.full-name-error').hide();
+		// }
 	});
 });
